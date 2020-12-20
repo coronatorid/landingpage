613d3cff85f73d857600336da5d21167dd4adf63
insomnius <aliana.arib@gmail.com>
2020-12-21 01:44:44 +0700

    I've had enough with javascript shit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div class="flex-shrink-0">
-        <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
-      </div>
-      <div>
-        <div class="text-xl font-medium text-black">ChitChat</div>
-        <p class="text-gray-500">You have a new message!</p>
-      </div>
-    </div>
+    <Nav/>
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav'
+
 export default {
   name: 'Coronator',
-  components: {}
+  components: {
+    Nav
+  }
 }
 </script>
