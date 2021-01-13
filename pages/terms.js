0d2e237843e672791d28bab3375476c418a25a50
insomnius <aliana.arib@gmail.com>
2021-01-13 23:58:15 +0700

    TOS update

--- pages/terms.js
+++ pages/terms.js
@@ -1,37 +1,27 @@
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
 import Head from 'next/head';
-// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import gfm from 'remark-gfm';
-
-// const CodeBlock = ({ language, value }) => {
-//   return (
-//     <SyntaxHighlighter showLineNumbers={true} language={language}>
-//       {value}
-//     </SyntaxHighlighter>
-//   );
-// };
 
 const Blog = ({ content, data }) => {
   const frontmatter = data;
 
   return (
     <>
-    <Head>
+      <Head>
         <title>{frontmatter.title} | Coronatorasd - Indonesia Covid Application</title>
-    </Head>
-    <section className="section">
+      </Head>
+      <section className="section">
         <div className="container max-w-screen-md">
-            <h1>{frontmatter.title}</h1>
-            <br/>
-            <ReactMarkdown
-                plugins={[gfm]}
-                escapeHtml={false}
-                source={content}
-                // renderers={{ code: CodeBlock }}
-            />
+          <h1>{frontmatter.title}</h1>
+          <br />
+          <ReactMarkdown
+            plugins={[gfm]}
+            escapeHtml={false}
+            source={content}
+          />
         </div>
-    </section>
+      </section>
     </>
   );
 };
