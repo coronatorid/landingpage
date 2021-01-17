bd3aa0bfd0f55c1d964f47ec9d4fc520df59691b
Rudi Setiawan <rudestewing@gmail.com>
2021-01-17 11:06:34 +0700

    Ref: change array to object to better indexing type

--- components/Badge.js
+++ components/Badge.js
@@ -1,34 +1,35 @@
 import PropTypes from 'prop-types'
 
 const Badge = ({type, children}) => {
-  const allowedTypes = [
-    {
+  const allowedTypes = {
+    default: {
       name: 'default',
       classes: 'bg-gray-100 border ',
     },
-    {
+    danger: {
       name: 'danger',
       classes: 'bg-red-600 text-gray-100',
     },
-    {
+    primary: {
       name: 'primary',
       classes: 'bg-blue-600 text-gray-100',
     },
-    {
+    secondary: {
       name: 'secondary',
       classes: 'bg-gray-300 text-white',
     },
-    {
+    success: {
       name: 'success',
       classes: 'bg-green-600 text-gray-100',
     },
-    {
+    warning: {
       name: 'warning',
       classes: 'bg-yellow-300 text-gray-800',
     },
-  ];
+  };
 
   function selectedType() {
+    return allowedTypes[type] ?? allowedTypes['default']
     const _type = allowedTypes.find((allowedType) => {
       return allowedType.name == type
     })
--- components/BadgeReportStatus.js
+++ components/BadgeReportStatus.js
@@ -2,30 +2,24 @@ import Badge from './Badge'
 import PropTypes from 'prop-types'
 
 const BadgeReportStatus = ({status}) => {
-  const allowedStatuses = [
-    {
-      id: 0,
+
+  const allowedStatuses = {
+    0: {
       name: 'Dikonfirmasi',
       type: 'danger'
     },
-    {
-      id: 1,
+    1: {
       name: 'Ditolak',
-      type: 'secondary',
+      type: 'secondary'
     },
-    {
-      id: 2,
+    2: {
       name: 'Pending',
-      type: 'warning',
-    },
-  ]
+      type: 'warning'
+    }
+  }
 
   function selectedStatus() {
-    const _status = allowedStatuses.find((allowedStatus) => {
-      return allowedStatus.id == status
-    })
-
-    return _status ? _status : allowedStatuses[0]
+    return allowedStatuses[status] ?? allowedStatuses[0]
   }
 
   return (
